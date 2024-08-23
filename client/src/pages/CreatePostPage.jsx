889aff1422610d0ce9aefd49689dc962b3146d97
omkar861856 <omkar861856@gmail.com>
2024-08-23 16:42:19 +0530

    layout added

---
+++ client/src/pages/CreatePostPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import createPostForm from '../components/ui/createPostForm'
+
+const CreatePostPage = () => {
+  return (
+    <div>
+        <h1>Create your post here</h1>
+        <createPostForm />
+    </div>
+  )
+}
+
+export default CreatePostPage
